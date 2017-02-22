9f3a7745fe7efe915c73c21e5af7e678e5cce926
evanronnei <etronnei@mtu.edu>
2017-02-22 17:51:32 -0500

    Added board generation

---
+++ Darklight 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public TileManager tileScript;
+	
+	private int level = 0;
+	
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+		tileScript = GetComponent<TileManager>();
+		InitGame();
+	}
+	
+	void InitGame() {
+		tileScript.SetupScene(level);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
