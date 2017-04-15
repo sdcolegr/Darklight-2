c9290394bf9476bb5cdcee6019e88d0be28a8a62
evanronnei <etronnei@mtu.edu>
2017-04-15 17:48:37 -0400

    Added single enemy that follows player

---
+++ Darklight 2/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+	public GameObject player;
+	public float speed = 2f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards(transform.position,player.transform.position, speed*Time.deltaTime);
+	}
+}
