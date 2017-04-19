60ca59d7686bd2b716563ecb4db36967a118bc0c
Zach <zsphilli@mtu.edu>
2017-04-19 00:14:12 -0400

    menu

---
+++ Darklight 2/Assets/Scripts/loadOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadOnClick : MonoBehaviour {
+	public GameObject loadingImage;
+	
+	public void LoadScene(int wave) {
+		loadingImage.SetActive(true);
+		SceneManager.LoadScene (wave);	
+	}
+}
